d641917656e84e3a3839e759101802ad3885f76c
rihanas <rihanna.socials@gmail.com>
2024-09-27 14:06:01 +0300

    update temporary changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './assets/scss/main.scss';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
+
+  // </React.StrictMode>
 );
