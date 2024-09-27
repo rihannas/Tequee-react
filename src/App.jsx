4723a180cd6ce8064004e00b627d8b5efb37a871
rihanas <rihanna.socials@gmail.com>
2024-09-27 14:06:34 +0300

    fixed router and added debugging statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import {
+  Route,
+  Routes,
+  BrowserRouter as Router,
+  useLocation,
+} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Nav/Navbar';
 import ServicePage from './pages/ServicePage/ServicePage';
 import SellerPage from './pages/SellerPage/SellerPage';
-import ServiceDetails from './components/ServiceDetails/ServiceDetails';
+import ServiceDetailPage from './pages/ServiceDetailsPage/ServiceDetailsPage';
+
+function RoutesWithLogging() {
+  const location = useLocation();
+  console.log('Current path:', location.pathname);
+
+  return (
+    <Routes>
+      <Route
+        path='/service'
+        element={<ServicePage />}
+      />
+      <Route
+        path='/service/:id'
+        element={<ServiceDetailPage />}
+      />
+      <Route
+        path='/seller/:id'
+        element={<SellerPage />}
+      />
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <>
-              <ServicePage />
-            </>
-          }
-        />
-        <Route
-          path='/service/:id'
-          element={<ServiceDetails />}
-        />
-        <Route
-          path='/seller/:id'
-          element={<SellerPage />}
-        />
-      </Routes>
+      <RoutesWithLogging />
     </Router>
   );
 }
