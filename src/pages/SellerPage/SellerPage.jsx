811ddddb5501c77abb18ae990bb9c95b82299c65
rihanas <rihanna.socials@gmail.com>
2024-09-30 08:21:02 +0300

    remodified to have components

--- src/pages/SellerPage/SellerPage.jsx
+++ src/pages/SellerPage/SellerPage.jsx
@@ -1,52 +1,14 @@
 import styles from './SellerPage.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faStar,
-  faStore,
-  faLocationDot,
-  faUserGroup,
-} from '@fortawesome/free-solid-svg-icons';
+
+import ProfileInfo from '../../components/Profile/ProfileInfo';
 
 const SellerPage = () => {
   return (
     <div className={styles.sellerpage_container}>
-      <div className={styles.top}>
-        <div className={styles.left}></div>
-        <div className={styles.right}>
-          <h4>status</h4>
-          <ul>
-            <li>
-              <FontAwesomeIcon
-                icon={faStar}
-                className={styles.icon}
-              />
-              Ratings
-            </li>
-            <li>
-              <FontAwesomeIcon
-                icon={faStore}
-                className={styles.icon}
-              />
-              Total orders
-            </li>
-            <li>
-              <FontAwesomeIcon
-                icon={faLocationDot}
-                className={styles.icon}
-              />
-              Location
-            </li>
-            <li>
-              <FontAwesomeIcon
-                icon={faUserGroup}
-                className={styles.icon}
-              />
-              Followed by
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className={styles.bottom}>f</div>
+      <ProfileInfo />
+      {/* Portfolio */}
+      {/* Top reviews */}
+      {/* services */}
     </div>
   );
 };
