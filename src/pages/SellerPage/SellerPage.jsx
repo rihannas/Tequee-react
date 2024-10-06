0fad4a7b15bae3f62fd565b312e4a7bb826066d2
rihanas <rihanna.socials@gmail.com>
2024-10-07 01:24:47 +0300

    Refactor SellerPage to add profile info, thumbnail, top reviews, and seller services

--- src/pages/SellerPage/SellerPage.jsx
+++ src/pages/SellerPage/SellerPage.jsx
@@ -1,14 +1,30 @@
 import styles from './SellerPage.module.scss';
 
 import ProfileInfo from '../../components/Profile/ProfileInfo';
+import Thumbnail from '../../components/Thumbnail/Thumbnail';
+import TopReview from '../../components/ReviewSection/TopReview';
+import SellerService from '../../components/Service/SellerService';
 
 const SellerPage = () => {
   return (
     <div className={styles.sellerpage_container}>
       <ProfileInfo />
-      {/* Portfolio */}
-      {/* Top reviews */}
-      {/* services */}
+      <div className={styles.middle}>
+        <Thumbnail className={styles.Thumbnail} />
+
+        <div className={styles.review_container}>
+          <h3>Top Reviews</h3>
+          <TopReview />
+          <TopReview />
+        </div>
+      </div>
+
+      <h1>Seller services</h1>
+      <div className={styles.seller_services}>
+        <SellerService />
+        <SellerService />
+        <SellerService />
+      </div>
     </div>
   );
 };
