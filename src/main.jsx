32d49446061c47fa6aacb3e519660e419dc59fae
rihanas <rihanna.socials@gmail.com>
2024-11-10 04:04:06 +0300

    remove clerk

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import './assets/scss/main.scss';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
